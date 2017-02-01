87006d72a13e4cdec69cefb413be959149f9819e
Parth Jansari <parthjansari@outlook.com>
2017-02-01 20:50:27 +0530

    switched to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { routes } from './app.router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
