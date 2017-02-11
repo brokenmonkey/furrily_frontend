5a64bb10a2927408a339146aeeac88f4865a73d3
parth181195 <parthjansari@outlook.com>
2017-02-11 19:23:26 +0530

    search bar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {   
+  trigger,
+  style,
+  animate,
+  transition,
+  state,
+  Component, 
+  OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -6,16 +13,31 @@ declare var $;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+    animations: [
+      trigger('search' , [
+      state('hide', style({
+        left: "100%"
+      })),
+      state('show', style({
+        left: 0
+      })),
+      transition('hide => show', animate('200ms ease-out')),
+      transition('show => hide', animate('200ms ease-out'))
+  , ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'app works!';
   shouldShowNav = true;
-
+  state: string = 'hide';
   constructor(private router: Router) {
 
   }
-
+  search_toggle() {
+    this.state = (this.state === 'hide' ? 'show' : 'hide');
+    console.log(this.state);
+  }
   ngOnInit() {
     $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 300
